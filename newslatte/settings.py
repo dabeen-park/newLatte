34916fec8ea3bbb6ce82fabd19f9ed2626681e8a
dabeen-park <gamgyut777@sogang.ac.kr>
2022-08-19 22:44:48 +0900

    연결되라

--- newslatte/settings.py
+++ newslatte/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'DJANGO_SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 
 ALLOWED_HOSTS = ['*']
